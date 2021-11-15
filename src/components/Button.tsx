1c2bd077425b376a6e56f02e06aee5adc893804b
4233532 - Gustavo R. Ferreira <gustavor.ferreira@se.df.gov.br>
2021-11-15 18:36:58 -0300

    [ADD] - Projeto NWL com React e Firebase.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,12 @@
-import {useState} from "react";
+import { ButtonHTMLAttributes } from "react";
 
-export function Button() {
-    const [counter, setCounter] = useState(0);
+import '../styles/button.scss'
 
-    function increment() {
-        setCounter(counter + 1);
-    }
+type BottonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-    function decrement() {
-        setCounter(counter - 1);
-    }
+export function Button(props: BottonProps) {
 
     return (
-        <button onClick={increment}>{counter}</button>
+        <button className="button" {...props} />
     )
 }
