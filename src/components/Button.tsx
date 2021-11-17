33a5a938c18bc8c4c8a4abcc81cad4159adcb4fc
4233532 - Gustavo R. Ferreira <gustavor.ferreira@se.df.gov.br>
2021-11-17 04:40:23 -0300

    [ADD] - Projeto NWL com React e Firebase. Parte 4: parte de comentários reaproveitado e um compnente para a área admin.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,12 @@ import { ButtonHTMLAttributes } from "react";
 
 import '../styles/button.scss'
 
-type BottonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type BottonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    isOutline?: boolean;
+};
 
-export function Button(props: BottonProps) {
-
+export function Button({ isOutline = false, ...props }: BottonProps) {
     return (
-        <button className="button" {...props} />
+        <button className={`button ${isOutline ? 'outlined' : ''}`} {...props} />
     )
 }
---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,83 @@
+import { FormEvent, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Button } from "../components/Button";
+import { RoomCode } from "../components/RoomCode";
+import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
+import { Question } from "../components/Question";
+import { useRoom } from "../hooks/useRoom";
+
+import logoImg from '../assets/images/logo.svg';
+
+import '../styles/room.scss';
+
+type RoomParams = {
+    id: string;
+}
+
+export function AdminRoom() {
+    const { user } = useAuth();
+    const [newQuestion, setNewQuestion] = useState('');
+    const params = useParams<RoomParams>();
+    const roomId = params.id;
+    const { questions, title } = useRoom(roomId);
+
+    async function handleSendQuestion(event: FormEvent) {
+        event.preventDefault();
+
+        if (newQuestion.trim() === '') {
+            return;
+        }
+
+        if (!user) {
+            throw new Error('You must be logged in');
+        }
+
+        const question = {
+            content: newQuestion,
+            author: {
+                name: user.name,
+                avatar: user.avatar
+            },
+            isHighlighted: false,
+            isAnswered: false
+        }
+
+        await database.ref(`/rooms/${roomId}/questions`).push(question);
+
+        setNewQuestion('');
+    }
+
+    return(
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <div>
+                        <RoomCode code={roomId}/>
+                        <Button isOutline>Encerrar sala</Button>
+                    </div>
+                </div>
+            </header>
+            <main>
+                <div className="room-title">
+                    <h1>Sala {title}</h1>
+                    { questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
+
+                </div>
+
+                <div className="question-list">
+                    {questions.map(question => {
+                        return(
+                            <Question
+                                key={question.id}
+                                content={question.content}
+                                author={question.author}
+                            />
+                        );
+                    })}
+                </div>
+            </main>
+        </div>
+    )
+}
