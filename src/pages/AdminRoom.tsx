671219adf7c616ca18abed51a2627d094efdc8e8
4233532 - Gustavo R. Ferreira <gustavor.ferreira@se.df.gov.br>
2021-11-17 06:41:48 -0300

    [ADD] - Projeto NWL com React e Firebase. Parte 7: botão encerrar do admin em funcionamento.

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,11 +1,14 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { Button } from "../components/Button";
 import { RoomCode } from "../components/RoomCode";
 // import { useAuth } from "../hooks/useAuth";
 import { Question } from "../components/Question";
 import { useRoom } from "../hooks/useRoom";
+import { database } from "../services/firebase";
 
 import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
+
 
 import '../styles/room.scss';
 
@@ -15,9 +18,24 @@ type RoomParams = {
 
 export function AdminRoom() {
     // const { user } = useAuth();
+    const history = useHistory();
     const params = useParams<RoomParams>();
     const roomId = params.id;
     const { questions, title } = useRoom(roomId);
+
+    async function handleEndRoom() {
+        database.ref(`/rooms/${roomId}`).update({
+            closedAt: new Date(),
+        });
+
+        history.push('/');
+    }
+
+    async function handleDeleteQuestion(questionId: string) {
+        if (window.confirm('Você tem certeza que deseja excluir esta pergunta?')) {
+            await database.ref(`/rooms/${roomId}/questions/${questionId}`).remove();
+        }
+    }
 
     return(
         <div id="page-room">
@@ -26,7 +44,7 @@ export function AdminRoom() {
                     <img src={logoImg} alt="Letmeask" />
                     <div>
                         <RoomCode code={roomId}/>
-                        <Button isOutline>Encerrar sala</Button>
+                        <Button isOutline onClick={handleEndRoom}>Encerrar sala</Button>
                     </div>
                 </div>
             </header>
@@ -44,7 +62,14 @@ export function AdminRoom() {
                                 key={question.id}
                                 content={question.content}
                                 author={question.author}
-                            />
+                            >
+                                <button
+                                    type="button"
+                                    onClick={() => handleDeleteQuestion(question.id)}
+                                >
+                                    <img src={deleteImg} alt="Remover pergunta" />
+                                </button>
+                            </Question>
                         );
                     })}
                 </div>
