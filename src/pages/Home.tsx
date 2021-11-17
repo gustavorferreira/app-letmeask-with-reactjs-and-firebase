ee0c0709dc3cf08ffa797c5ecbd5df1f2b8640cb
4233532 - Gustavo R. Ferreira <gustavor.ferreira@se.df.gov.br>
2021-11-17 03:29:38 -0300

    [ADD] - Projeto NWL com React e Firebase. Parte 2: implementação da Sala com recursos Realtime Firebase tornando o envio de perguntas dinâmicas e em tempo real.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,21 @@
 import { useHistory } from 'react-router-dom';
 import { Button } from "../components/Button";
 import { useAuth } from "../hooks/useAuth";
+import { FormEvent, useState } from "react";
+import { database } from "../services/firebase";
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
 import '../styles/auth.scss';
+import {wait} from "@testing-library/react";
+
 
 
 export function Home() {
     const history = useHistory();
+    const [roomCode, setRoomCode] = useState('')
     const { user, signWithGoogle } = useAuth();
     
     async function handleCreateRoom() {
@@ -19,6 +24,23 @@ export function Home() {
         }
 
         history.push('/rooms/new');
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`/rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Room does not exists.');
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`);
     }
 
     return(
@@ -38,10 +60,12 @@ export function Home() {
                     <div className="separator">
                         ou entre em uma sala
                     </div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Entrar na sala
